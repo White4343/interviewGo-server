82fce38e8a0f05c4d17c43fabb4923e68ee640cb
Andrii Tarasenko <96845911+White4343@users.noreply.github.com>
2022-10-27 22:08:10 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
     user: "postgres",
-    password: "taras1968",
+    password: "",
     host: "localhost",
     port: 5432,
-    database: "interviewgodb"
+    database: ""
 })
 
 module.exports = pool
